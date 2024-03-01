9d1229bbedc54ff1afd022ee49bc864e883f5678
Byron Montaño <byronmb0407@gmail.com>
2024-03-01 11:19:07 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MainPage());
+    return const MaterialApp(home: MainPage());
   }
 }
 
